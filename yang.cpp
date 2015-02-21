0eb0db00a76be147360ac4df9290010fc7ee017b
Yang Yang <YYang@Yangs-MacBook-Pro.local>
2015-02-20 18:52:25 -0600

    a C++ code that does nothing

---
+++ yang.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+-   yang.cpp
+-   Prog Tools for Scits Engrs - CIVL 8903 Homework IV
+-
+-   Homework Discription:
+-   Demonstrate that you have secured access to the computer resources necessa-
+-   ry to be successful in hte course. Also, perfect your use of function.
+-
+-   Author: Yang Yang
+-
+-   Git Repository:
+******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+// main function
+int main() {
+
+    return 0;
+
+}
