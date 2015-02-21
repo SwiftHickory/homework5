2d43548c3b54d22a966bd71b12fb85fd56a66858
Yang Yang <YYang@Yangs-MacBook-Pro.local>
2015-02-20 19:06:44 -0600

    modify code to produce simple message

--- yang.cpp
+++ yang.cpp
@@ -8,7 +8,7 @@
 -
 -   Author: Yang Yang
 -
--   Git Repository:
+-   Github Repository: https://github.com/SwiftHickory/homework4.git
 ******************************************************************************/
 
 #include <iostream>
@@ -17,6 +17,13 @@ using namespace std;
 
 // main function
 int main() {
+
+    // produce a simple paragraphy messege
+    cout << "I was able to compile this code using the HPC at the University of Memphis. ";
+    cout << "When I compiled it there, it did not produce any warning message. ";
+    cout << "The HPC uses a GNU C++ compiler that can be considered a good up-to-date standard. ";
+    cout << "I also version-controlled this code using git, and used a remoterepository hosted by github. a";
+    cout << "If I can do this, so can you!!!" << endl;
 
     return 0;
 
