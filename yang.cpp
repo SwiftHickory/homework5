2f8d8b90e34cdb11974a9a79db7b120cff9f2fba
Yang Yang <YYang@Yangs-MBP.memphis.edu>
2015-02-24 17:42:25 -0600

    my final version

--- yang.cpp
+++ yang.cpp
@@ -23,6 +23,8 @@ using namespace std;
 const string errorFileName = "yang.err";
 ofstream errorFile;
 
+typedef unsigned long long int ullong;
+
 // function to open input file
 void openInput(ifstream &inputFile, string fileName);
 
@@ -35,8 +37,20 @@ void errorMessage(const string &message);
 // print messeage to both terminal and a file
 void printOutput(ofstream &outputFile, const string &message);
 
-// function to produce first 10*n number of Fibonacci sequence
-void fibonacciSequence(int numberOfRaw, ofstream &outputFile);
+// function to produce first num number of fibonacci senquence using int
+bool fibonacciSequenceInt(int num, ullong *seq);
+
+// function to produce first num number of fibonacci senquence using double
+void fibonacciSequenceDouble(int num, double *seq);
+
+// print out the fibonacci sequence of int
+void printFibonacciInt(int num, ullong *seq, ofstream &outputFile);
+
+// print out the fibonacci sequence of double
+void printFibonacciDouble(int num, double *seq, ofstream &outputFile);
+
+// determine number of digits in a positive integer
+int numOfDigits(ullong num);
 
 // main function
 int main() {
@@ -46,7 +60,8 @@ int main() {
     const string inputFileName = "yang.in";
     const string outputFileName = "yang.out";
     ostringstream messageStream;  // used for output to both file and terminal
-    int n;  // 10*n is totoal numbers to compute
+    int n;  // 10*n is total numbers to compute
+    int m;  // total numbers to compute
 
     // produce a simple paragraphy messege
     messageStream << "I was able to compile this code using the HPC at the University of Memphis. ";
@@ -59,20 +74,46 @@ int main() {
     messageStream << "I am so cool, that I was also able to write a code that produces the first ";
     messageStream << "M numbers of the Fibonacci sequence. Here they are: " << endl;
 
-    openOutput(errorFile, errorFileName);
-    openOutput(outputFile, outputFileName);
-    openInput(inputFile, inputFileName);
-
     // print the message to both terminal and error file
     printOutput(outputFile, messageStream.str());
 
+    openOutput(outputFile, outputFileName);
+    openInput(inputFile, inputFileName);
+
     // get the number need to compute
     inputFile >> n;
+    if (n < 1) {
+        errorMessage("Input file is wrong. Must be a positive number!\n");
+        exit(EXIT_FAILURE);
+    }
+    m = 10*n;
 
-    fibonacciSequence(n, outputFile);
+    // compute fibonacci sequence
+    //allocate memory for fibonacci sequecne
+    ullong *seqInt;
+    seqInt = new ullong [m];
+
+    // if the maximum fibonacci number does not exceed the limit of ullong
+    if (fibonacciSequenceInt(m, seqInt)) {
+        printFibonacciInt(m, seqInt, outputFile);
+
+        delete seqInt;
+    } else {
+        double *seqDouble;
+        seqDouble = new double [m];
+
+        fibonacciSequenceDouble(m, seqDouble);
+        printFibonacciDouble(m, seqDouble, outputFile);
+
+        delete seqDouble;
+
+        // print out error message
+        errorMessage("Warning: Number exceed the maximum integer. Use double instead!\n");
+    }
 
     inputFile.close();
     outputFile.close();
+
     // if open error file, close it
     if (errorFile.is_open()) {
         errorFile.close();
@@ -82,52 +123,80 @@ int main() {
 
 }
 
-// function to produce first 10*n number of Fibonacci sequence
-void fibonacciSequence(int numberOfRaw, ofstream &outputFile) {
+// function to produce first num number of fibonacci senquence using int
+bool fibonacciSequenceInt(int num, unsigned long long *seq) {
 
-    unsigned long long fn;  // F[n]
-    unsigned long long fn_1 = 1;  // F[n-1]
-    unsigned long long fn_2 = 0;  // F[n-2]
-    ostringstream outputStream;  // used for output to both file and terminal
-    int numberOfWidth = numberOfRaw > 10 ? 21 : 2*numberOfRaw + 2;  // used for setw
+    // initialize first two elements
+    seq[0] = 0;
+    seq[1] = 1;
 
-    outputStream << setw(numberOfWidth) << fn_2 << setw(numberOfWidth) << fn_1;
-
-    // the remaining 8 number in the first row
-    for (int j = 2; j < 10; j++) {
-        fn = fn_1 + fn_2;
-        fn_2 = fn_1;
-        fn_1 = fn;
-
-        outputStream << setw(numberOfWidth) << fn_1;
-    }
-    outputStream << endl;
-
-    // the remaining number
-    for (int i = 1; i < numberOfRaw; i++) {
-        for (int j = 0; j < 10; j++) {
-            // if the number exceed the maximum of the integer, stop computing and print error messege
-            if (ULLONG_MAX - fn < fn_1) {
-                outputStream << endl;
-                errorMessage("Warning: Stop! Number exceed the maximum integer.\n");
-                goto stop;
-            }
-
-            fn = fn_1 + fn_2;
-            fn_2 = fn_1;
-            fn_1 = fn;
-
-            outputStream << setw(numberOfWidth) << fn_1;
+    // compute the rest elements
+    for (int i = 2; i < num; i++) {
+        // if the number exceed the maximum of the integer, stop computing and return false
+        if (ULLONG_MAX - seq[i - 1] < seq[i - 2]) {
+            return false;
+        } else {
+            seq[i] = seq[i - 1] + seq[i - 2];
         }
-
-        outputStream << endl;
     }
 
-    stop:
-    // print the sequence
+    return true;
+
+}
+
+// function to produce first num number of fibonacci senquence using double
+void fibonacciSequenceDouble(int num, double *seq) {
+
+    // initialize first two elements
+    seq[0] = 0;
+    seq[1] = 1;
+
+    // compute the rest elements
+    for (int i = 2; i < num; i++) {
+        seq[i] = seq[i - 1] + seq[i - 2];
+    }
+
+}
+
+// print out the fibonacci sequence of int
+void printFibonacciInt(int num, ullong *seq, ofstream &outputFile) {
+
+    int numberOfWidth = numOfDigits(seq[num - 1]) + 1;  // used for setw
+    const int numbersPerLine = 10;
+    ostringstream outputStream;  // used for output to both file and terminal
+
+    for (int i = 0; i < num; i++) {
+
+        outputStream << setw(numberOfWidth) << seq[i];
+
+        // print \n every numbersPerLine numbers
+        if ((i + 1) % numbersPerLine == 0 ) {
+            outputStream << endl;
+        }
+    }
+
     printOutput(outputFile, outputStream.str());
 
-    return;
+}
+
+// print out the fibonacci sequence of double
+void printFibonacciDouble(int num, double *seq, ofstream &outputFile) {
+
+    int numberOfWidth = 15;  // used for setw
+    const int numbersPerLine = 10;
+    ostringstream outputStream;  // used for output to both file and terminal
+
+    for (int i = 0; i < num; i++) {
+
+        outputStream << setw(numberOfWidth) << scientific << seq[i];
+
+        // print \n every numbersPerLine numbers
+        if ((i + 1) % numbersPerLine == 0 ) {
+            outputStream << endl;
+        }
+    }
+
+    printOutput(outputFile, outputStream.str());
 
 }
 
@@ -139,9 +208,9 @@ void openInput(ifstream &inputFile, string fileName) {
     // perform sanity check it
     if (!inputFile.is_open()) {
         errorMessage("Cannot open input file: " + fileName + "\n");
+        exit(EXIT_FAILURE);
     }
 
-    return;
 }
 
 // function to open output file
@@ -153,7 +222,6 @@ void openOutput(ofstream &outputFile, string fileName) {
     if (!outputFile.is_open()) {
         if (fileName == errorFileName) {
             // if we can open error file, just print out to terminal
-            cout << "Cannot open output file. Error only printed to terminal!" << endl;
             cout << "Cannot open error file: " << errorFileName << endl;
         } else {
             errorMessage("Cannot open output file: " + fileName + "\n");
@@ -162,7 +230,6 @@ void openOutput(ofstream &outputFile, string fileName) {
         exit(EXIT_FAILURE);
     }
 
-    return;
 }
 
 // function to print message to errorFile using function printOutput
@@ -183,6 +250,18 @@ void printOutput(ofstream &outputFile, const string &message) {
     cout << message;
     outputFile << message;
 
-    return;
+}
+
+// determine number of digits in a positive integer
+int numOfDigits(ullong num) {
+
+    int digits = 0;
+
+    while (num) {
+        num /= 10;
+        digits++;
+    }
+
+    return digits;
 
 }
