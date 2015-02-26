e1575e3bb1af040812bb2f8de381e9242982e41c
Yang Yang <YYang@Yangs-MacBook-Pro.local>
2015-02-25 20:54:31 -0600

    Just create initial files

--- yang.cpp
+++ yang.cpp
@@ -1,14 +1,17 @@
 /******************************************************************************
 -   yang.cpp
--   Prog Tools for Scits Engrs - CIVL 8903 Homework IV
+-   Prog Tools for Scits Engrs - CIVL 8903 Homework V
 -
 -   Homework Discription:
--   Demonstrate that you have secured access to the computer resources necessa-
--   ry to be successful in hte course. Also, perfect your use of function.
+-   This assignment requires you to understand how to declare and manipulate a 
+-   collections of objects using a arrays, enumerators, and structures. You
+-   are expected to learn and to design and use functions in C++ using the ge-
+-   neral style of the examples done in class, including declaration of param-
+-   eters.
 -
 -   Author: Yang Yang
 -
--   Github Repository: https://github.com/SwiftHickory/homework4.git
+-   Github Repository: https://github.com/SwiftHickory/homework5.git
 ******************************************************************************/
 
 #include <iostream>
@@ -16,14 +19,11 @@
 #include <iomanip>
 #include <cstdlib>
 #include <sstream>
-#include <climits>
 
 using namespace std;
 
 const string errorFileName = "yang.err";
 ofstream errorFile;
-
-typedef unsigned long long int ullong;
 
 // function to open input file
 void openInput(ifstream &inputFile, string fileName);
